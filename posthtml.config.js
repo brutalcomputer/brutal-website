f7c922e0c4db4c4ceaa2b942f8fbd4adf69e153e
Jimmy Oliger <jimmyoliger@gmail.com>
2020-02-07 17:34:30 +0100

    First

---
+++ posthtml.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: {
+    "posthtml-expressions": {
+      locals: {
+        title: "BRUTAL COMPUTER",
+        description:
+          "BRUTAL COMPUTER is a technology studio based in Paris, France.",
+        twitter: "@brutalcomputer",
+        url: "https://brutal.computer"
+      }
+    }
+  }
+};
